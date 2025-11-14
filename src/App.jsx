a6930d268570fe4b0f5607635df82d28723708ff
Ade Ariawan <ariawan.ade@gmail.com>
2025-11-14 08:08:37 +0700

    color schemas update

--- src/App.jsx
+++ src/App.jsx
@@ -23,17 +23,17 @@ export default function App() {
   }, [kelas]);
 
   return (
-    <div className="min-h-screen bg-gray-50 text-gray-800">
+    <div className="min-h-screen bg-slate-600 text-gray-800">
       {/* HEADER */}
-      <header className="p-6 text-center bg-white shadow">
-        <h1 className="text-6xl uppercase font-bold text-cyan-600">
+      <header className="p-6 text-center bg-slate-900 shadow">
+        <h1 className="text-6xl uppercase font-bold text-white">
           Galeri Proyek Siswa
         </h1>
         {/* <p className="text-gray-500 mt-1">Kelas 11A · 11B · 11C</p> */}
       </header>
 
       {/* MAIN CONTENT */}
-      <main className="max-w-4xl mx-auto mt-8 p-6 bg-white shadow rounded-2xl">
+      <main className="max-w-4xl mx-auto mt-8 p-6 bg-white shadow-2xl rounded-2xl">
         <div className="flex flex-col md:flex-row gap-4 mb-6">
           <ClassSelector
             value={kelas}
@@ -55,7 +55,7 @@ export default function App() {
       </main>
 
       {/* FOOTER */}
-      <footer className="text-center text-sm text-gray-400 py-6">
+      <footer className="text-center text-sm text-gray-400 py-8">
         &copy; {new Date().getFullYear()} Ade Ariawan
       </footer>
     </div>

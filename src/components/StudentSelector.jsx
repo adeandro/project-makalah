55e024e055f97deea972b0538974b3af89758d89
Ade Ariawan <ariawan.ade@gmail.com>
2025-11-18 16:24:43 +0700

    little adjustment

--- src/components/StudentSelector.jsx
+++ src/components/StudentSelector.jsx
@@ -10,7 +10,7 @@ export default function StudentSelector({ value, onChange, students }) {
         {students.length === 0 ? "Memuat siswa..." : "Pilih Nama Siswa"}
       </option>
       {students.map((s) => (
-        <option key={s} value={s}>
+        <option style={{ textTransform: "capitalize" }} key={s} value={s}>
           {s}
         </option>
       ))}
